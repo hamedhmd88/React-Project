5706c4a6e0e8587e2fcdd639756dc10665745db3
Hamed Mahjoobi <hmdhamed88@gmail.com>
2025-11-22 22:56:46 +0330

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 function App() {
-  return <></>;
+  return <><div>
+    hello world</div></>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./styles/colors.css"
+import "./styles/shadows.css"
+import "./styles/shapes.css"
+import "./styles/typography.css"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
