9f21ba92398feb8a762a8a10859a11b635616a76
Hamed Mahjoobi <hmdhamed88@gmail.com>
2025-11-23 22:43:58 +0330

    add file and folder for board

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from "react";
+
+import { Outlet } from "react-router";
+
+import styles from "./RootLayout.module.css";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function RootLayout(): ReactNode {
+  return (
+    <div className={styles["root-layout"]}>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
